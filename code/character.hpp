3f42ba25d1a5a071f1ec9688622bd7ad41cc0ec7
taninhaa <mendesdiastania@gmail.com>
2022-12-15 20:33:14 +0100

    getters added

--- code/character.hpp
+++ code/character.hpp
@@ -7,6 +7,9 @@ class Character{
 public: 
     Character(std::string f, std::string n, std::string o) : first_name(f), last_name(n), origin(o), present(0){};
     std::string getName(){return first_name + ' '+ last_name;}
+    std::string getOrigin(){return origin;}
+    bool getPresence(){return present;}
+    void setPresence(){present=1;}
     virtual void indicate_position() const = 0;
     virtual bool on_field() const = 0; 
     virtual void move() const = 0;
