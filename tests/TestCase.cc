ce23ce5ec67b180a9e5eead35564799b0b5066f3
taninhaa <mendesdiastania@gmail.com>
2022-12-15 16:57:53 +0100

    Ajout testcase

---
+++ tests/TestCase.cc
@@ -0,0 +1,9 @@
+// Let Catch provide main():
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "character.hpp"
+#include "field.hpp"
+#include "goal.hpp"
+#include "referee.hpp"
+#include "team.hpp"
