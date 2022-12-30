3c4b486b2819f986c28fa2f9b2905917b164233e
taninhaa <mendesdiastania@gmail.com>
2022-12-30 21:56:22 +0100

    Ajout constructuer team

--- code/team.cpp
+++ code/team.cpp
@@ -8,41 +8,24 @@ Team::Team(string  country, string  players){
     int cpt=0;
     while (players[i] != '\0')
     {
-        if (players[i] != ',' && players[i] != ' ')
+        if (players[i] != ',')
         {
-            //cout << retour << endl;
             retour = retour + players[i];
-
         }
         
-        if ( players[i] == ',' && players[i+1]==' ' || players[i+1] == '\0'){
+        
+        if ( players[i] == ',' || players[i+1] == '\0'){
             
             if (retour!="")
             {
-                cout << retour << endl;
-               // cout << "ici a :" << cpt << endl;
+               
                 Player current_player (retour, country);
-                //cout << "Le nom est : " << current_player.getName() << endl;
-                //cpt=0;
+                cout << "Le nom est : " << current_player.getName() << endl;
+                roster.push_back(current_player);
                 
             }
-            if (players[i+1]==' '){
-                retour=retour + players[i+1];
-            }
-            //cout << retour << endl;
+          
             retour="";
-            /*
-            if (cpt==0 && retour !="" && players[i]==' '){
-            //cout << "cpt : "<< cpt << endl;
-            
-            cpt++;
-            cout << cpt << endl;
-            }
-            if (retour !=" " && cpt==1 || retour !=""){
-                cout << retour << endl;
-                retour= retour + ' ';
-            }
-            */
             
         }
 
